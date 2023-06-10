23be76a877e4ad9aa7e28eb32560d564c16b9599
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-06-10 17:51:34 -0500

    carino al login

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,76 +1,93 @@
-import React from 'react'
-import { Formik, Field, Form } from 'formik'
-import * as Yup from 'yup'
-import { Box, Button, TextField, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAuthUser } from '../../redux/actions'
-import axios from 'axios'
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAuthUser } from "../../redux/actions";
+import { Toaster, toast } from "sonner";
+import axios from "axios";
 
 const initialValues = {
-    email: '',
-    password: ''
-}
+  email: "",
+  password: "",
+};
 
 const validationSchema = Yup.object({
-    email: Yup.string().required('The e-mail is required.'),
-    password: Yup.string().required('The password is required.')
-})
+  email: Yup.string().required("Por favor ingrese su indentificación"),
+  password: Yup.string().required("Por favor ingrese su contraseña"),
+});
 
 export default function LoginForm() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const submitHandler = async ({ email, password }) => {
-        try {
-            await axios.post(`/session`, { email, password });
-            dispatch(getAuthUser(email))
-            navigate('/dashboard')
-        } catch ({ response }) {
-            alert('User or Password is Incorrect! Please try again.')
-        }
+  const submitHandler = async ({ email, password }) => {
+    try {
+      await axios.post(`/session`, { email, password });
+      dispatch(getAuthUser(email));
+      navigate("/dashboard");
+    } catch ({ response }) {
+      toast("La identificación o la contraseña son incorrectos 😳");
+      // alert('La identificación o la contraseña son incorrectos 😳')
     }
+  };
 
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={submitHandler}
-        >
-            {(formik) => {
-                const { errors, touched, isSubmitting } = formik;
-                return (
-                    <Form>
-                        <Box sx={{ display: "flex", flexDirection: "column", margin: "20px" }}>
-                            <Field
-                                sx={{ marginBottom: "20px" }}
-                                placeholder='E-mail'
-                                name='email'
-                                as={TextField}
-                                error={errors.email && touched.email}
-                                helperText={(errors.email && touched.email) ? errors.email : null}
-                            />
-                            <Field
-                                placeholder='Password'
-                                type="password"
-                                name='password'
-                                as={TextField}
-                                error={errors.password && touched.password}
-                                helperText={(errors.password && touched.password) ? errors.password : null}
-                            />
-                            <Typography onClick={() => { navigate('/password-recovery') }} sx={{
-                                cursor: "pointer", marginBottom: "20px", '&:hover': {
-                                    color:"blue",
-                                    textDecoration:"underline"
-                                },
-                            }}>Forgot your password?</Typography>
-                            <Button type="submit" variant='contained' disabled={isSubmitting}>
-                                Log In
-                            </Button>
-                        </Box>
-                    </Form>
-                )
-            }}
-        </Formik>
-    )
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={submitHandler}
+    >
+      {(formik) => {
+        const { errors, touched, isSubmitting } = formik;
+        return (
+          <Form>
+            <Box
+              sx={{ display: "flex", flexDirection: "column", margin: "20px" }}
+            >
+              <Field
+                sx={{ marginBottom: "20px" }}
+                placeholder="E-mail"
+                name="email"
+                as={TextField}
+                error={errors.email && touched.email}
+                helperText={errors.email && touched.email ? errors.email : null}
+              />
+              <Field
+                placeholder="Password"
+                type="password"
+                name="password"
+                as={TextField}
+                error={errors.password && touched.password}
+                helperText={
+                  errors.password && touched.password ? errors.password : null
+                }
+              />
+              {/* <Typography
+                onClick={() => {
+                  navigate("/password-recovery");
+                }}
+                sx={{
+                  cursor: "pointer",
+                  marginBottom: "20px",
+                  "&:hover": {
+                    color: "blue",
+                    textDecoration: "underline",
+                  },
+                }}
+              >
+                Forgot your password?
+              </Typography> */}
+              <Button type="submit" variant="contained" disabled={isSubmitting}
+              sx={{mt: 3, backgroundColor: "purple"}}
+              >
+                Log In
+              </Button>
+            </Box>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 }
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Grid, Typography, Box } from '@mui/material'
-import Login from '../../components/Login/Login.jsx'
+import React from "react";
+import { Grid, Typography, Box } from "@mui/material";
+import Login from "../../components/Login/Login.jsx";
 
 const styles = {
   principal: {
@@ -8,7 +8,7 @@ const styles = {
     justifyContent: "center",
     aligntItems: "center",
     fontWeight: "bold",
-    border: "1px solid",
+    //border: "1px solid",
     width: "100vw",
     height: "100vh",
   },
@@ -17,26 +17,48 @@ const styles = {
     justifyContent: "center",
     aligntItems: "center",
     fontWeight: "bold",
-    border: "1px solid",
+    //border: "1px solid",
     width: "100vw",
     height: "100vh",
-  }
-}
+  },
+};
 
 export default function Landing() {
   return (
     <Box>
       <Grid container sx={styles.principal}>
-        <Grid item xs={12} md={6} order={{ xs: 2, md: 1 }}>
-          <Typography sx={{ alignSelf: "center", fontWeight: "bold", fontSize: "20px" }}>Aquí va lo decorativo/visual de la landing</Typography>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          order={{ xs: 2, md: 1 }}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Grid item width={"90%"} sx={{ boxShadow: 10 }}>
+            <img
+              src="https://res.cloudinary.com/dbxsr9mfc/image/upload/v1682391978/calixto/Cover_lrghp5.jpg"
+              style={{ objectFit: "contain" }}
+              width={"100%"}
+              sx={{ boxShadow: "15" }}
+            ></img>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6} order={{ xs: 1, md: 2 }} sx={{ border: "1px solid", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          order={{ xs: 1, md: 2 }}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Login />
         </Grid>
       </Grid>
-      <Box sx={styles.secondary}>
-        <Typography sx={{ alignSelf: "center", fontWeight: "bold", fontSize: "20px" }}>Resto de lo decorativo/informacional</Typography>
-      </Box>
     </Box>
-  )
+  );
 }
