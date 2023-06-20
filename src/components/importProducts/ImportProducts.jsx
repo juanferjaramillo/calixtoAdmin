b881e6bb9e68a764c6f2214902cf5b3ff0685ceb
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-06-19 22:15:24 -0500

    csv patch

--- src/components/importProducts/ImportProducts.jsx
+++ src/components/importProducts/ImportProducts.jsx
@@ -10,9 +10,10 @@ import { Typography } from "@mui/material";
 //====================COMPONENT=======================
 const ImportPoducts = () => {
   const [loader, setLoader] = useState(false);
-  const fileRef = useRef();
+  const loadRef = useRef();
+  const updateRef = useRef();
 
-  const handleFileUpload = (event) => {
+  const handleLoadFile = (event) => {
     const file = event.target.files[0];
     Papa.parse(file, {
       header: true,
@@ -20,12 +21,30 @@ const ImportPoducts = () => {
         setLoader(true);
         try {
           await axios.post("/bulkcreate", result.data);
-          alert("import finished");
+          alert("Importación finalizada");
         } catch (error) {
-          alert("error importing data");
+          alert("Error importando la data");
         }
         setLoader(false);
         event.target.value = null; //resets the file input
+      },
+    });
+  };
+
+  const handleUpdateFile = () => {
+    const file = event.target.files[0];
+    Papa.parse(file, {
+      header: true,
+      complete: async (result) => {
+        setLoader(true);
+        try {
+          await axios.patch("/updateProducts", result.data);
+          alert("Actualización finalizada");
+        } catch (error) {
+          alert("Error actualizando la información");
+        }
+        setLoader(false);
+        event.target.value = null;
       },
     });
   };
@@ -36,23 +55,45 @@ const ImportPoducts = () => {
       <input
         type="file"
         accept=".csv"
-        ref={fileRef}
-        onChange={handleFileUpload}
+        ref={loadRef}
+        onChange={handleLoadFile}
         style={{ display: "none" }} //hides the input element
-        id="csv-input"
+        id="csv-load"
+      />
+
+      <input
+        type="file"
+        accept=".csv"
+        ref={updateRef}
+        onChange={handleUpdateFile}
+        style={{ display: "none" }}
+        id="csv-update"
       />
 
       <Button
         sx={{ margin: "1vw" }}
         variant="contained"
         onClick={
-          () => fileRef.current.click()
+          () => loadRef.current.click()
           //triggers a click on the element (the input) opening the file selector por uploading
         }
         color="primary"
       >
         Importar Productos
       </Button>
+
+      <Button
+        sx={{ margin: "1vw" }}
+        variant="contained"
+        onClick={
+          () => updateRef.current.click()
+          //triggers a click on the element (the input) opening the file selector por uploading
+        }
+        color="primary"
+      >
+        Actualizar Productos
+      </Button>
+
       {loader ? <Typography>Loading Data...</Typography> : null}
     </Box>
   );
