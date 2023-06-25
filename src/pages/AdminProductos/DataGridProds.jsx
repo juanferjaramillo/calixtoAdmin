75f35b5fe05bc4903299acade4458243c7c8ddb4
juanferjaramillo <juanfer.jaramillo@gmail.com>
2023-06-25 17:19:46 -0500

    full datagrid

--- src/pages/AdminProductos/DataGridProds.jsx
+++ src/pages/AdminProductos/DataGridProds.jsx
@@ -1,31 +1,26 @@
-import Box from "@mui/material/Box"
-import Grid from "@mui/material/Grid"
-import { DataGrid } from '@mui/x-data-grid';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { DataGrid } from "@mui/x-data-grid";
 import { useSelector } from "react-redux";
 
-
 const columns = [
-  { field: 'id', headerName: 'CÓDIGO', width: 70 },
-  { field: 'nombre', headerName: 'PRODUCTO', width: 130 },
-  { field: 'precioBase', headerName: 'PRECIO', width: 130 }]
- 
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
+  { field: "id", headerName: "CÓDIGO", width: 100 },
+  { field: "nombre", headerName: "PRODUCTO", width: 300 },
+  { field: "precioBase", headerName: "PRECIO", width: 130},
+  { field: "existencia", headerName: "EXISTENCIA", width: 130 },
+  { field: "rotacion", headerName: "ROTACION", width: 130 },
+  { field: "agotado", headerName: "AGOTADO", width: 130 },
+  { field: "limitado", headerName: "LIMITADO", width: 130 },
+
+];
+
+//=====================COMPONENT=========================
 
 export default function DataGridProds() {
-  const rows = useSelector(state=>state.filteredProducts);
+  const rows = useSelector((state) => state.filteredProducts);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
